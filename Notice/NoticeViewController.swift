3e2ee5871017acce39ecfef13ef062c75ef38a7e
kim-sunwoong <sw649666@gmail.com>
2021-09-30 16:19:31 +0900

    공지사항 기능 테스트 앱

---
+++ Notice/NoticeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NoticeViewController.swift
+//  Notice
+//
+//  Created by 김선웅 on 2021/09/27.
+//
+
+import UIKit
+
+class NoticeViewController: UIViewController {
+    
+    var noticeContent: (title: String, detail: String, date:String)?
+    @IBOutlet weak var noticeView: UIView!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        noticeView.layer.cornerRadius = 6
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        
+        guard let noticeContent = noticeContent else {
+            return
+        }
+        titleLabel.text = noticeContent.title
+        detailLabel.text = noticeContent.detail
+        dateLabel.text = noticeContent.date
+    }
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
